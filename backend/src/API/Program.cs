b7a40b4948f4216f43d015d524afb654fe455629
Sahil Rashid | MAQ Software <sahil.rashid@maqsoftware.com>
2026-05-18 09:45:19 +0530

    feat(db): Add swagger packaage and check the connection to db. If https://localhost:xxxx/health returns (healthy): ok.

--- backend/src/API/Program.cs
+++ backend/src/API/Program.cs
@@ -2,15 +2,16 @@
 using AINotion.API.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-
+using Microsoft.AspNetCore.Routing;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 builder.Services.AddControllers();
 builder.Services.AddInfrastructureServices(builder.Configuration);
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
-
+app.MapHealthChecks("/health");
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();

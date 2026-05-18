bc4a4135892ae0a4f6e6251da5371b5e43eac0a0
Sahil Rashid | MAQ Software <sahil.rashid@maqsoftware.com>
2026-05-18 09:19:40 +0530

    feat(db): Added the postre conection string in windows via - ConnectionStrings__DefaultConnection, which is interpreted as ConnectionString::_DefaultConnection by the OS

--- backend/src/API/obj/Debug/net10.0/API.AssemblyInfo.cs
+++ backend/src/API/obj/Debug/net10.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b690a8aafaf624518831212cd7533eb89a29224")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37ae84fc9a192e48a7b461a18ecf5c3354820ea6")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
